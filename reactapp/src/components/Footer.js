17c66c120211845ae51590f476b4bcac1365df01
Ashwinram005 <ashwinrammohan2005@gmail.com>
2025-08-21 20:28:00 +0530

    Changes

--- reactapp/src/components/Footer.js
+++ reactapp/src/components/Footer.js
@@ -1,6 +1,4 @@
 // src/components/Footer.js
-import React from "react";
-
 export default function Footer() {
   return (
     <footer className="bg-gray-900 text-gray-100 text-center py-4 px-2 text-sm font-medium shadow-inner rounded-t-lg">
